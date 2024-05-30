d5ba5c1873a963e9b7fa6a7a3e7f5eb6531c6f29
kanan <bayramli-kenan@mail.ru>
2024-05-30 23:53:27 +0400

    PhotoController/PhotoSave endpoint

---
+++ Services/PhotoStock/PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,39 @@
+﻿using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhotoStock.Dtos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)  
+        {
+            if (photo!=null && photo.Length>0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);  //cancellation token eger PhotoSave sonlansa, browseri baglasalar ya cixsalar saytimizdan, bu endpointde ishini dayandirsin save elememsin. exeption atib dayandirir
+
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDto photoDto = new() {Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
+        }
+    }
+}
