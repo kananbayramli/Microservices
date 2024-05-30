af8da48923208df02baad5f1f58d365c36b3b68f
kanan <bayramli-kenan@mail.ru>
2024-05-30 20:02:42 +0400

    UserController/GetUser endpoint

--- IdentityServer/FreeCourse.Identity/Config.cs
+++ IdentityServer/FreeCourse.Identity/Config.cs
@@ -54,7 +54,7 @@ namespace FreeCourse.Identity
                     ClientSecrets = { new Secret("secret".Sha256())},
                     AllowOfflineAccess = true,
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes = {IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, "roles"},
+                    AllowedScopes = {IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName, "roles" },
                     AccessTokenLifetime = 1*60*60,
                     RefreshTokenExpiration = TokenExpiration.Absolute,
                     AbsoluteRefreshTokenLifetime = (int) (DateTime.Now.AddDays(60) - DateTime.Now).TotalSeconds,
