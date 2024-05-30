67946a5d3ed2b8b7d608322b6fd4982925da32ca
kanan <bayramli-kenan@mail.ru>
2024-05-31 00:04:08 +0400

    PhotoController / PhotoDelete endpoint

--- Services/PhotoStock/PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/PhotoStock/Controllers/PhotosController.cs
@@ -35,5 +35,20 @@ namespace PhotoStock.Controllers
 
             return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
         }
+
+        [HttpPost]
+        public IActionResult PhotoDelete(string photoUrl) 
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo Not Found", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
     }
 }
