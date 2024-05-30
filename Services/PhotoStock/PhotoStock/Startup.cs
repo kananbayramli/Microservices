18d7740e5bd2990f7c019cb55e41867f6f490286
kanan <bayramli-kenan@mail.ru>
2024-05-31 00:18:43 +0400

    PhotoStockAPI(microservice) Authorization

--- Services/PhotoStock/PhotoStock/Startup.cs
+++ Services/PhotoStock/PhotoStock/Startup.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,8 +27,18 @@ namespace PhotoStock
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_catalog";
+                options.RequireHttpsMetadata = false;
+            });
 
-            services.AddControllers();
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter());  //butun controllara [Authorize] elave edecek. Bir bir hamsina yazmaq lazim deyil
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PhotoStock", Version = "v1" });
@@ -45,6 +57,7 @@ namespace PhotoStock
 
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
