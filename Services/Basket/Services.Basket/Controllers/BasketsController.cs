9523f09bee6fca27903e8586dfdafb3b7d4a6e62
kanan <bayramli-kenan@mail.ru>
2024-06-01 15:03:22 +0400

    Testing Basket APi

--- Services/Basket/Services.Basket/Controllers/BasketsController.cs
+++ Services/Basket/Services.Basket/Controllers/BasketsController.cs
@@ -27,6 +27,8 @@ namespace Services.Basket.Controllers
         [HttpGet]
         public async Task<IActionResult> GetBasket() 
         {
+            var claims = User.Claims;
+
             return CreateActionResultInstance(await _basketService.GetBasket(_sharedIdentityService.GetUserId));
         }
 
--- Services/Basket/Services.Basket/Startup.cs
+++ Services/Basket/Services.Basket/Startup.cs
@@ -15,6 +15,7 @@ using Services.Basket.Services;
 using Services.Basket.Settings;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -33,6 +34,8 @@ namespace Services.Basket
         public void ConfigureServices(IServiceCollection services)
         {
             var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub"); //sub-i maplemesin. No nameidentifier
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
